55ed28aa4cc14f5f5d68fb3516e2aabf936ee3ae
Praveen Kumar Jha <32644784+PraveenKumarJha1@users.noreply.github.com>
2018-05-17 19:50:08 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form</title>
+</head>
+<body>
+<form method="post" action="welcome.php">
+	name:<input type="text" name="name" ><br>
+	email:<input type="email" name="email" ><br>
+	<input type="submit">
+</form>
+
+
+</body>
+</html>
