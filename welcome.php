be6a4f16b4170604b3cce36909baf45688af9470
Praveen Kumar Jha <32644784+PraveenKumarJha1@users.noreply.github.com>
2018-05-17 19:49:42 +0530

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+Name of person is: <?php echo $_POST['name'];  ?> <br>
+Email of <?php echo $_POST['name']; echo " is ";	  echo $_POST['email'];  
+
+echo "<p> hello my name is: $_POST[name]  and my email is: $_POST[email] </p>";
+
+?>
+
+
+</body>
+</html>
